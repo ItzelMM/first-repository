a7e62a03f1ada8f644bf0b757d164c47c70db02a
ItzelMM <85587774+ItzelMM@users.noreply.github.com>
2021-06-20 21:41:00 -0500

    Update CodingProblem2.py

--- HW2/CodingProblem2.py
+++ HW2/CodingProblem2.py
@@ -2,6 +2,7 @@
 import datetime
 
 dates_f = open("inputDates.txt", "r")
+right_dates = open("parsedDates.txt", "w")
 dates = []
 months_num = {"january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7,
               "august": 8, "september": 9, "october": 10, "november": 11, "december": 12}
@@ -21,5 +22,7 @@ for date in dates:
         mdy[0] = months_num[mdy[0]]
         if datetime.date(int(mdy[2]), int(mdy[0]), int(mdy[1])) <= datetime.date.today():
             print(str(mdy[0]) + "/" + mdy[1] + "/" + mdy[2])
+            right_dates.write(str(mdy[0]) + "/" + mdy[1] + "/" + mdy[2] + '\n')
 
 dates_f.close()
+right_dates.close()
